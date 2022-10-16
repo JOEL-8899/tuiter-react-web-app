a84218ec4457f4b6d3d76b852a670c231e70e647
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-10-16 16:42:50 -0400

    a5 JavaScript sp22

--- public/labs/tuiter/PostSummaryList/index.js
+++ public/labs/tuiter/PostSummaryList/index.js
@@ -2,7 +2,7 @@ import PostSummaryItem from "./PostSummaryItem.js";
 import exploreItems from "./postSummary.js"
 const PostSummaryList = () => {
     return (`
-    <ul class="list-group">
+    <ul class="list-group ms-5">
       ${exploreItems.map(item => {
         return PostSummaryItem(item);
     }).join('')
