824e77666858ee15f6ea6a46d3d00ad06a4adb5c
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-10-16 21:58:36 -0400

    a5 JavaScript sp22

--- public/labs/tuiter/PostList/index.js
+++ public/labs/tuiter/PostList/index.js
@@ -2,7 +2,7 @@ import posts from "./post.js"
 import PostItem from "./postItem.js";
 const PostList = () => {
     return (`
-    <div class="pe-5 ps-2">
+    <div>
       ${posts.map(item => {
         return PostItem(item);
     }).join('')
