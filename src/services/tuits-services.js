1df3c8daeba2e439e01814958b20afb75e30639f
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-11-20 18:50:50 -0500

    A8 commit

--- src/services/tuits-services.js
+++ src/services/tuits-services.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-// const API_BASE = process.env.REACT_APP_API_BASE;
-// const TUITS_API = `${API_BASE}/tuits`;
-const TUITS_API = "https://tuiter-node-apps.herokuapp.com/api/tuits";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
+//const TUITS_API = "https://tuiter-node-apps.herokuapp.com/api/tuits";
 export const createTuit = async (tuit) =>{
 
     const response = await axios.post(TUITS_API,tuit);
